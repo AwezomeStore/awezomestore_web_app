1bae1cfbe5b30d111c48554a0d76c9a2e319147b
gtzambranop <gtzambranop@unal.edu.co>
2022-01-19 06:53:16 -0500

    basic product target

---
+++ src/components/ProductTarget.vue
@@ -0,0 +1,67 @@
+<template>
+  <div class="col-md-6 col-lg-4 col-xl-3 product-card-container">
+  <b-card title="Title" img-src="https://placekitten.com/g/300/450" img-alt="Image" img-top
+    class="product-card">
+    <b-card-text>
+      This is a wider card with supporting text below as a natural lead-in to additional content.
+      This content is a little bit longer.
+    </b-card-text>
+    <div class="rating-product">
+      <b-form-rating v-model="value" readonly></b-form-rating>
+      <p class="rating-info">{{ product.rating.toFixed(1) }}</p>
+    </div>
+  </b-card>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'ProductTarget',
+  props: {
+    product: Object
+  }
+}
+</script>
+
+<style>
+.product-card-container {
+  padding: .3rem;
+}
+
+.product-card-container .product-card{
+  background-color: var(--gray-1);
+  padding: .5rem;
+}
+
+.product-card .card-img-top {
+  width: auto;
+  margin: auto;
+  max-width: 100%;
+
+  max-height: 20rem;
+}
+
+.product-card .card-body {
+  padding: 1rem .5rem;
+}
+
+.rating-product {
+  display: flex;
+}
+
+.rating-product .b-rating {
+  padding: 0rem;
+  background-color: transparent;
+  border: none;
+  width: 50%;
+}
+
+.b-rating  .b-rating-icon {
+  color: var(--blue-1);
+}
+
+.rating-info {
+  margin: .5rem;
+}
+
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,23 +1,45 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="home-content">
+    <div class="col-md-2">
+    Component of sections
+    </div>
+    <div class="col-md-10 list-target-products">
+      <ProductTarget :product=product></ProductTarget>
+      <ProductTarget :product=product></ProductTarget>
+      <ProductTarget :product=product></ProductTarget>
+      <ProductTarget :product=product></ProductTarget>
+      <ProductTarget :product=product></ProductTarget>
+      <ProductTarget :product=product></ProductTarget>
+    </div>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import ProductTarget from '@/components/ProductTarget.vue'
 
 export default {
   name: 'Home',
   components: {
-    HelloWorld
+    ProductTarget
+  },
+  data () {
+    return {
+      product: {rating: 4.235}
+    }
   }
 }
 </script>
 <style scoped>
-#home{
-  background: #202020;
+.home-content{
+  background: var(--gray-2);
+  padding: 1rem 0.5rem;
+  display: flex;
 }
+
+.list-target-products {
+  display: flex;
+  flex-wrap: wrap;
+}
+
 </style>
